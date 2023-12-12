b7cce705bd24a57ab7cfe6c8407477b6506d7352
JustWinstonIsOK <JustWinstonIsOK@protonmail.com>
2023-12-12 09:56:46 -0500

    initial commit

---
+++ apply_patches.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo Hard-resetting all submodules...
+git submodule foreach git reset --hard > /dev/null
+patch_core () {
+    CORENAME="$1"
+    echo
+    echo $CORENAME:
+    # skip the core-specific patch if it doesn't exist
+    [[ -f patches/"$CORENAME".patch ]] || shift
+    for PATCHNAME in "$@"; do
+        echo Applying $PATCHNAME.patch...
+        git apply --directory="$CORENAME"_MiSTer patches/"$PATCHNAME".patch
+    done
+    if [[ -f patches/"$CORENAME".pll.patch ]]; then
+        echo Applying $CORENAME.pll.patch...
+        git apply --whitespace=nowarn --directory="$CORENAME"_MiSTer patches/"$CORENAME".pll.patch
+    fi 
+}
+
+patch_core MegaCD ascal video_mixer
+patch_core MegaDrive ascal
+patch_core N64 ascal
+patch_core NES ascal
+patch_core PSX ascal
+patch_core SNES ascal video_mixer
+patch_core TurboGrafx16 ascal video_mixer
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+
+QUARTUS_SH=/c/intelFPGA_lite/17.0/quartus/bin64/quartus_sh
+
+mkdir -p out
+
+for CORE_DIRECTORY in $(find -maxdepth 1 -type d -name "*_MiSTer"); do
+    cd "$CORE_DIRECTORY"
+    QPF_NAMES=($(ls *.qpf))
+    RBF_NAME=$(basename ${QPF_NAMES[0]} .qpf)
+    "$QUARTUS_SH" --flow compile "$RBF_NAME" > ../out/"$RBF_NAME".log
+    cp -f output_files/"$RBF_NAME".rbf ../out
+    cd ..
+done
